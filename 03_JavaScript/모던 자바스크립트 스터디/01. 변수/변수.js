5b8b7b5bf4242e48aa5650084df44737777571b4
wlsdud0448 <wlsdud0448@naver.com>
2024-04-17 16:25:17 +0900

    0417_js

---
+++ 03_JavaScript/모던 자바스크립트 스터디/01. 변수/변수.js
@@ -0,0 +1,15 @@
+// 변수는 하나의 값을 저장한다.
+
+var userId = 1;
+var userName = 'Jang';
+
+var user = { id: 1, name: 'Jang'};
+
+var users = [
+    { id: 1, name: 'Jang'},
+    { id: 2, name: 'Kim'}
+]
+
+    console.log(users);
+    
+
