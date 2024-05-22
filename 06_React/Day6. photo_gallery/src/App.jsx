02ab0b107e0341605c17811b1fb002e9158c5399
wlsdud0448 <wlsdud0448@naver.com>
2024-05-22 14:25:47 +0900

    0521_react

---
+++ 06_React/Day6. photo_gallery/src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+
+import './App.css';
+import Gallery from './components/Gallery';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Unsplash 갤러리 📷</h1>
+            <p>원하는 이미지를 검색하세요.</p>
+            <Gallery />
+        </div>
+    );
+}
+
+export default App;
