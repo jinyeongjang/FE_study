29dc9f52fc873a6673a3bd308ecd3bd58b27514d
wlsdud0448 <wlsdud0448@naver.com>
2024-06-03 22:56:03 +0900

    0603_react

--- 06_React/Redux_TDD/redux_tdd/src/App.js
+++ 06_React/Redux_TDD/redux_tdd/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [counter, setCounter] = useState(0);
+    const [disabled, setDisabled] = useState(false);
+
+    return (
+        <div className="App">
+            <div className="app-wrap">
+                <h1>Redux TDD 카운터 앱</h1>
+                <header className="App-header">
+                    <h3 data-testid="counter" className="counter">
+                        {counter}
+                    </h3>
+                    <div className="button-group">
+                        <div>
+                            <button class="button-count" data-testid="minus-button" onClick={() => setCounter((count) => count - 1)} disabled={disabled}>
+                                감소
+                            </button>
+                            <button class="button-count" data-testid="plus-button" onClick={() => setCounter((count) => count + 1)} disabled={disabled}>
+                                증가
+                            </button>
+                        </div>
+
+                        <div>
+                            <button className="off-button" data-testid="on/off-button" onClick={() => setDisabled((prev) => !prev)}>
+                                켜기/끄기
+                            </button>
+                        </div>
+                    </div>
+                </header>
+            </div>
+        </div>
+    );
 }
 
 export default App;
